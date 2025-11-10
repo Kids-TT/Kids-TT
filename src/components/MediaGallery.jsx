291fa8a14558db8e6ca644dd99ee67c45fb77b0e
jacob <aoqnwnd@naver.com>
2025-11-10 19:25:34 +0900

    [fix] 이미지 크기 비율 변경

--- src/components/MediaGallery.jsx
+++ src/components/MediaGallery.jsx
@@ -5,19 +5,13 @@ function MediaGallery({ items }) {
   return (
     <div className="media-gallery">
       {items.map((item) => {
-        if (item.type === 'image') {
-          return (
-            <section key={item.id} className="media-section">
-              <div className="media-section__image-wrapper">
-                <img src={item.src} alt={item.alt} className="media-section__image" />
-              </div>
-            </section>
-          );
-        }
-
         return (
           <section key={item.id} className="media-section">
-            <YouTubeEmbed title={item.title} embedUrl={item.src} />
+            {item.type === 'image' ? (
+              <img src={item.src} alt={item.alt} className="media-section__image" />
+            ) : (
+              <YouTubeEmbed title={item.title} embedUrl={item.src} />
+            )}
           </section>
         );
       })}
